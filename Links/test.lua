5bd964a5e464049c2dbfc13e67997dadfd8e5256
Kai-aus-Hannover <31270915+Kai-aus-Hannover@users.noreply.github.com>
2017-09-26 07:57:06 +0200

    Create test.lua

---
+++ Links/test.lua
@@ -0,0 +1,39 @@
+function links(datei, a)
+    local output = io.open(string.sub(datei, 1, a - 5) .. "-LINKS.txt", "r")
+    if output then
+        if string.len(io.open(string.sub(datei, 1, a - 5) .. "-LINKS.txt", "r"):read("*a")) > 0 then
+            return
+        end
+    end
+    local input = io.open(datei, "r")
+    output = io.open(string.sub(datei, 1, a - 5) .. "-LINKS.txt", "w")
+    for zeile in input:lines() do
+        local nix, anfang, ende, link, beschreibung
+        nix, anfang = string.find(zeile, '<A HREF="')
+        if anfang then
+            ende = string.find(zeile, '"', anfang + 1)
+            if ende then
+                link = string.sub(zeile, anfang + 1, ende - 1)
+            end
+        end
+        nix, anfang = string.find(zeile, '>', ende)
+        if anfang then
+            ende = string.find(zeile, '<', anfang)
+            if ende then
+                beschreibung = string.sub(zeile, anfang + 1, ende - 1)
+            end
+        end
+        if link and beschreibung then
+            output:write(string.format([==[<a href="%s">%s</a>]==] .. "\n" , link, beschreibung))
+        end
+    end
+    input:close()
+    output:close()
+end
+
+for datei in io.popen([[dir "E:\Transfer" /s /b /a]]):lines() do
+    local a = string.len(datei)
+    if string.sub(datei, a - 4, a) == ".html" then
+        print(pcall(links, datei, a))
+    end
+end
